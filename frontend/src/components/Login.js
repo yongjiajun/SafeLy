990bf58e214498ce46d200f52f7b294628f75330
Austin <rockmanvnx6@gmail.com>
2019-04-13 16:25:44 +1000

    almost done select time

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,7 +6,7 @@ export default class Login extends Component{
     constructor() {
         super();
         this.state = {
-            toRegister: false,
+            toRegister: true,
         }
         this.renderChild = this.renderChild.bind(this);
     }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,20 +1,74 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import '../assets/register.css'
 export default class Register extends Component{
     constructor() {
         super();
+        this.state = {
+            toChild: true,
+        }
+        this.renderChild = this.renderChild.bind(this);
+    }
+    renderChild() {
+        this.setState({toChild: true});
     }
     render() {
-        return (
-            <div className = "register">
-                <h1 className="login-title">
-                    <FontAwesomeIcon icon={faMapMarkerAlt} />
-                    SafeLy
-                </h1>
-                Register
-            </div>
+        if(this.state.toChild == false) {
+                return (
+                    <div className = "register">
+                        <h1 className="logo-title">
+                            <FontAwesomeIcon icon={faMapMarkerAlt} />
+                            SafeLy
+                        </h1>
+                        <div className = "container">
+                            <div className = "header">
 
-        );
+                                <label>Sign up</label>
+                            </div>
+                            <div className = "form">
+                                <div className = "element-inline">
+                                    <div>
+                                        <label>First name</label>
+                                        <input type="text"></input>
+                                    </div>
+                                    <div>
+                                        <label>Last name</label>
+                                        <input type="text"></input>
+                                    </div>
+                                </div>
+                                <div className = "element">
+                                    <label>E-mail</label>
+                                    <input type="email"></input>
+                                </div>
+                                <div className="element">
+                                    <label>Password</label> 
+                                    <input type ="password"></input>
+                                </div>
+                                <div className="element">
+                                    <label>Confirm Password</label>
+                                    <input type="password"></input>
+                                </div>
+                                <div className="element divider">
+                                    <label>University</label>
+                                    <select name="uni">
+                                        <option value="rmit">RMIT</option>
+                                        <option value="unimelb">University of Melbourne</option>
+
+                                    </select>
+                                </div>
+
+                                <div className="element">
+                                    <button type="button" class="bigBtn blueTheme" onClick={this.renderChild}>Sign up</button>
+                                </div>
+
+                            </div>
+                            
+                        </div>
+                    </div>
+            );
+        } else {
+            return React.cloneElement(React.Children.only(this.props.children), {});
+        }
     }
 }
